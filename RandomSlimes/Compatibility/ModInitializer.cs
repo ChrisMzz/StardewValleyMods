a4d5efa87549c68b0a899ae8552a3dd8a768f16a
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2024-03-29 15:56:15 +0100

    Added RandomSlimes mod

---
+++ RandomSlimes/Compatibility/ModInitializer.cs
@@ -0,0 +1,80 @@
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
+
+namespace RandomSlimes.Compatibility
+{
+    public class ModInitializer
+    {
+        private readonly IManifest modManifest;
+        private readonly IModHelper helper;
+
+        public ModInitializer(IManifest modManifest, IModHelper helper)
+        {
+            this.modManifest = modManifest;
+            this.helper = helper;
+        }
+
+        public void Initialize(IGenericModConfigMenuApi api, ModConfig config)
+        {
+            api.Register(
+                mod: modManifest,
+                reset: () =>
+                {
+                    config = new ModConfig();
+                    ModEntry.Config = config;
+                },
+                save: () => helper.WriteConfig(config)
+            );
+
+            api.AddBoolOption(
+                mod: modManifest,
+                getValue: () => config.InWoods,
+                setValue: value => config.InWoods = value,
+                name: () => "Activate in Woods",
+                tooltip: () => "Check this to activate the mod in the Woods."
+            );
+
+            api.AddBoolOption(
+                mod: modManifest,
+                getValue: () => config.InSlimeArea,
+                setValue: value => config.InSlimeArea = value,
+                name: () => "Activate in Slime floors",
+                tooltip: () => "Check this to activate the mod in Slime floors in the Mines and Skull Caverns."
+            );
+
+            api.AddBoolOption(
+                mod: modManifest,
+                getValue: () => config.InQuarryArea,
+                setValue: value => config.InQuarryArea = value,
+                name: () => "Activate in Quarry Mines",
+                tooltip: () => "Check this to activate the mod in the Quarry Mines Area."
+            );
+
+            api.AddBoolOption(
+                mod: modManifest,
+                getValue: () => config.InSkullCaverns,
+                setValue: value => config.InSkullCaverns = value,
+                name: () => "Activate in Skull Caverns",
+                tooltip: () => "Check this to activate the mod in the Skull Caverns."
+            );
+
+            api.AddBoolOption(
+                mod: modManifest,
+                getValue: () => config.InRegularMines,
+                setValue: value => config.InRegularMines = value,
+                name: () => "Activate in Mines",
+                tooltip: () => "Check this to activate the mod in regular floors in the Mines."
+            );
+
+
+            api.AddBoolOption(
+                mod: modManifest,
+                getValue: () => config.Anywhere,
+                setValue: value => config.Anywhere = value,
+                name: () => "Activate anywhere",
+                tooltip: () => "Check this to activate the mod in any location of the game. Overrides other options."
+            );
+            
+        }
+    }
+}
---
+++ RandomSlimes/ModConfig.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+
+namespace RandomSlimes
+{
+    public class ModConfig
+    {
+        public bool InWoods { get; set; } = false;
+        public bool InSlimeArea { get; set; } = true;
+        public bool InQuarryArea { get; set; } = true;
+        public bool InSkullCaverns { get; set; } = true;
+        public bool InRegularMines { get; set; } = true;
+
+        public bool Anywhere { get; set; } = false;
+
+
+    }
+}
