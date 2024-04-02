45353e9712b33cd6e89e8d1f28c48603538cd622
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2024-04-02 14:17:59 +0200

    ModConfig was set to impossible value

--- SlimeMinerals/ModConfig.cs
+++ SlimeMinerals/ModConfig.cs
@@ -10,7 +10,7 @@ namespace SlimeMinerals
 
         public bool Anywhere { get; set; } = false;
 
-        public int Range { get; set; } = 30;
+        public int Range { get; set; } = 20;
 
     }
 }
